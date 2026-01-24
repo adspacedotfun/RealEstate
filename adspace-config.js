eb72f3d9fcc25c8cef304f59769aeff39bd163da
adspacedotfun <adspace.fun@gmail.com>
2026-01-24 13:21:26 +0530

    Update adspace-config.js

--- adspace-config.js
+++ adspace-config.js
@@ -5,7 +5,7 @@ const ADSPACE_CONFIG = {
   CONTRACT_ADDRESS: "0x40FF408E7cDd96a500463425f72b0829FEd9614c",
   RPC_ENDPOINT:
     "https://eth-sepolia.g.alchemy.com/v2/6tKAFNcLQN_P7X_JD2NZl0u3HHZtH_dp",
-  IPFS_GATEWAY: "https://cloudflare-ipfs.com/ipfs/",
+  IPFS_GATEWAY: "https://bronze-funny-scorpion-792.mypinata.cloud/ipfs",
   REFRESH_INTERVAL: 30000, // 30 seconds
 
   // ⚠️ IMPORTANT: Replace with your Space ID from adspace.fun dashboard
